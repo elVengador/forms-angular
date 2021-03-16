3ce8ebc644586cbf2330aac53fdd4810cb33c10f
elVengador <jimynicanor1001@gmail.com>
2021-03-16 16:39:03 -0500

    input + select with full validations

---
+++ src/app/pages/template/template.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { map } from 'rxjs/operators';
+import { PaisService } from 'src/app/services/pais.service';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+
+  usuario:any = {
+    nombre:'',
+    apellidos:'',
+    email:'',
+    pais:''
+  }
+  paises:any[] = []
+  
+
+  constructor(private paisService:PaisService) { }
+
+  ngOnInit(): void {
+    this.paisService.getPaises()
+      .subscribe(paises => this.paises = [
+        {name:'[ Selecciones pais ]',code:''},
+        ...paises
+      ])
+  }
+
+  save(form:NgForm){
+    console.log(form)
+  }
+
+}
